1afc79ff2ad1e166ddb748142ff231fef3ed6168
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-14 09:34:41 +0000

    application_controllerへdevise設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # devise利用の機能が使われる場合、その前にconfigure_perimitted_prametersが実行される。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  # nameのデータ操作を許可するアクションメソッドが指定されている。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end 
 end
