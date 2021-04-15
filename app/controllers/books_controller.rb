dc92d243433ae3dc673b465bc74a70015a537360
lvjiiiiu <lv_jiiiiu@yahoo.co.jp>
2021-04-15 23:42:55 +0000

    [新規作成]books_create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,25 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save 
+      redirect_to book_path(@book), notice: "You have created book successfully"
+    else
+      @books = Book.all
+      render "index"
+    end 
+  end 
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+  
 end
